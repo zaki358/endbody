09d66bd2fe0cfe4db9466693c8a2733dfab2ea8e
zaki358 <t.yamazaki.1129@gmail.com>
2022-09-25 00:17:22 +0900

    スライダー実装

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper(".mySwiper", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
