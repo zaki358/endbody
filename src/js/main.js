e7de991a03e63239108bdec34a9f79015b43bfd5
zaki358 <t.yamazaki.1129@gmail.com>
2022-10-18 23:44:28 +0900

    ざっくり完成

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,4 @@
+//swiper初期化
 var swiper = new Swiper(".mySwiper", {
   navigation: {
     nextEl: ".swiper-button-next",
@@ -10,7 +11,7 @@ var swiper = new Swiper(".mySwiper", {
 
 
 
-
+//ページのトップに戻るボタン（機能）
 const pageTopBtn = document.querySelector(".c-btn__footer--page-top");
 
 pageTopBtn.addEventListener("click", function () {
@@ -30,10 +31,11 @@ window.addEventListener("scroll", function () {
 
 
 
-
+//スクロール時下からスッと出てくる
 const fadeIn1 = document.querySelector(".p-service__section--training");
 const fadeIn2 = document.querySelector(".p-service__section--food");
 const fadeIn3 = document.querySelector(".p-introduction__area--trainer");
+const fadeIn4 = document.querySelector(".p-introduction__area--staff");
 
 const cb = function (entries, observer) {
   entries.forEach(entry => {
@@ -53,12 +55,51 @@ const options = {
   rootMargin: "0px",
   threshold: 0
 }
+
+const io = new IntersectionObserver(cb, options);
+io.observe(fadeIn1);
+io.observe(fadeIn2);
+io.observe(fadeIn3);
+io.observe(fadeIn4);
+
+
+//トレーナーのタブ
+const trainerList = document.querySelector(".p-introduction__list--trainer");
+const staffList = document.querySelector(".p-introduction__list--staff");
+const trainer = document.querySelector(".p-introduction__area--trainer");
+const staff = document.querySelector(".p-introduction__area--staff");
+
 document.addEventListener("DOMContentLoaded", function () {
-  const io = new IntersectionObserver(cb, options);
-  io.observe(fadeIn1);
-  io.observe(fadeIn2);
-  io.observe(fadeIn3);
+  trainerList.classList.add("js-tab");
+  trainer.classList.add("js-tab");
 })
 
+function addClass(el1, el2, el3, el4) {
+  el1.classList.add("js-tab");
+  el2.classList.remove("js-tab");
+  el3.classList.add("js-tab");
+  el4.classList.remove("js-tab");
+}
+
+trainerList.addEventListener("click", function () {
+  addClass(trainerList, staffList, trainer, staff);
+});
+
+staffList.addEventListener("click", function () {
+  addClass(staffList, trainerList, staff, trainer);
+});
 
 
+//スクロール時ヘッダー表示
+const header = document.querySelector(".p-header");
+const btnHeader = document.querySelector(".c-btn__header");
+
+window.addEventListener("scroll", function () {
+  if (window.pageYOffset > 100) {
+    header.style.backgroundColor = "white";
+    btnHeader.style.display = "block";
+  } else if (window.pageYOffset < 100) {
+    header.style.backgroundColor = "transparent";
+    btnHeader.style.display = "none";
+  }
+})
